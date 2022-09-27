be9dca250e473de9037cf28138d2b2a8556b8826
M. Sathis Kumar <73054487+mskumarsk@users.noreply.github.com>
2022-09-27 17:03:06 +0530

    Add files via upload

--- PlayGame.java
+++ PlayGame.java
@@ -1,123 +1,195 @@
-package Game;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class PlayGame {
-
-    public static void main(String[] args){
-
-        PlayGame playGame=new PlayGame();
-        playGame.m();
+    public static void main(String[] args) {
+        PlayGame playGame = new PlayGame();
+        playGame.play();
     }
-    public void m(){
-        String[][] array={{"   "," | ","   "," | ","   "},
-                {"---","-+-","---","-+-","---"},
-                {"   "," | ","   "," | ","   "},
-                {"---","-+-","---","-+-","---"},
-                {"   "," | ","   "," | ","   "}};
 
-        int[] computer=new int[5];
-        int[] player=new int[5];
-        int playerInput=0;
-        int computerInput=0;
-        String symbol="";
-        while (true) {
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    System.out.print(array[i][j]);
-                }
-                System.out.println();
+    public void play() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int n3 = scan.nextInt();
+        int n4 = scan.nextInt();
+        System.out.print("Enter target :");
+        int target = scan.nextInt();
+
+        char[] operators = {'+', '-', '*'};
+        int[] integers = {n1, n2, n3, n4};
+
+        int iLoop = 0;
+        int jLoop = 0;
+        int kLoop = 0;
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) {
+                iLoop = (n1 + n2);
+            } else if (i == 1) {
+                iLoop = (n1 - n2);
+            } else if (i == 2) {
+                iLoop = (n1 * n2);
             }
-            System.out.println("Enter the number (1-9) :");
-            Scanner scanner = new Scanner(System.in);
-            int number = scanner.nextInt();
-            boolean tf=checkNumber(number,computer,player);
-            if (tf){
-                player[playerInput]=number;
-                symbol=" X ";
-                addToArray(array,number,symbol);
-                for (int i = 0; i < array.length; i++) {
-                    for (int j = 0; j < array[i].length; j++) {
-                        System.out.print(array[i][j]);
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    jLoop = iLoop + n3;
+                } else if (j == 1) {
+                    jLoop = iLoop - n3;
+                } else if (j == 2) {
+                    jLoop = iLoop * n3;
+                }
+                for (int k = 0; k < 3; k++) {
+                    if (k == 0) {
+                        if (jLoop + n4 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n4);
+                        }
+                    } else if (k == 1) {
+                        if (jLoop - n4 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n4);
+                        }
+                    } else if (k == 2) {
+                        if (jLoop * n4 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n4);
+                        }
                     }
-                    System.out.println();
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) {
+                iLoop = (n4 + n3);
+            } else if (i == 1) {
+                iLoop = (n4 - n3);
+            } else if (i == 2) {
+                iLoop = (n4 * n3);
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    jLoop = iLoop + n2;
+                } else if (j == 1) {
+                    jLoop = iLoop - n2;
+                } else if (j == 2) {
+                    jLoop = iLoop * n2;
+                }
+                for (int k = 0; k < 3; k++) {
+                    if (k == 0) {
+                        if (jLoop + n1 == target) {
+                            System.out.println(n4 + "" + (operators[i]) + "" + n3 + (operators[j]) + "" + n2 + (operators[k]) + "" + n1);
+                        }
+                    } else if (k == 1) {
+                        if (jLoop - n1 == target) {
+                            System.out.println(n4 + "" + (operators[i]) + "" + n3 + (operators[j]) + "" + n2 + (operators[k]) + "" + n1);
+                        }
+                    } else if (k == 2) {
+                        if (jLoop * n1 == target) {
+                            System.out.println(n4 + "" + (operators[i]) + "" + n3 + (operators[j]) + "" + n2 + (operators[k]) + "" + n1);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) {
+                iLoop = (n4 + n2);
+            } else if (i == 1) {
+                iLoop = (n4 - n2);
+            } else if (i == 2) {
+                iLoop = (n4 * n2);
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    jLoop = iLoop + n3;
+                } else if (j == 1) {
+                    jLoop = iLoop - n3;
+                } else if (j == 2) {
+                    jLoop = iLoop * n3;
+                }
+                for (int k = 0; k < 3; k++) {
+                    if (k == 0) {
+                        if (jLoop + n1 == target) {
+                            System.out.println(n4 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n1);
+                        }
+                    } else if (k == 1) {
+                        if (jLoop - n1 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n1);
+                        }
+                    } else if (k == 2) {
+                        if (jLoop * n1 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n2 + (operators[j]) + "" + n3 + (operators[k]) + "" + n1);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (i == 0) {
+                iLoop = (n1 + n4);
+            } else if (i == 1) {
+                iLoop = (n1 - n4);
+            } else if (i == 2) {
+                iLoop = (n1 * n4);
+            }
+            for (int j = 0; j < 3; j++) {
+                if (j == 0) {
+                    jLoop = iLoop + n3;
+                } else if (j == 1) {
+                    jLoop = iLoop - n3;
+                } else if (j == 2) {
+                    jLoop = iLoop * n3;
+                }
+                for (int k = 0; k < 3; k++) {
+                    if (k == 0) {
+                        if (jLoop + n2 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n4 + (operators[j]) + "" + n3 + (operators[k]) + "" + n2);
+                        }
+                    } else if (k == 1) {
+                        if (jLoop - n2 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n4 + (operators[j]) + "" + n3 + (operators[k]) + "" + n2);
+                        }
+                    } else if (k == 2) {
+                        if (jLoop * n2 == target) {
+                            System.out.println(n1 + "" + (operators[i]) + "" + n4 + (operators[j]) + "" + n3 + (operators[k]) + "" + n2);
+                        }
+                    }
+                }
+            }
+        }
+
+        iLoop = 0;
+        jLoop = 0;
+
+        for (int i = 0; i < 1; i++) {
+            for (int j = 2; j < 4; j++) {
+
+                if (i == 0 && j == 2) {
+                    iLoop = Integer.parseInt(String.valueOf(integers[i]) + String.valueOf(integers[j]));
+                    jLoop = Integer.parseInt(String.valueOf(integers[i + 1]) + String.valueOf(integers[j + 1]));
+                } else if (i == 1 && j == 2) {
+                    iLoop = Integer.parseInt(String.valueOf(integers[i]) + String.valueOf(integers[j]));
+                    jLoop = Integer.parseInt(String.valueOf(integers[i - 1]) + String.valueOf(integers[j + 1]));
+                } else if (i == 0 && j == 3) {
+                    iLoop = Integer.parseInt(String.valueOf(integers[i]) + String.valueOf(integers[j]));
+                    jLoop = Integer.parseInt(String.valueOf(integers[i + 1]) + String.valueOf(integers[j - 1]));
+                } else if (i == 1 && j == 3) {
+                    iLoop = Integer.parseInt(String.valueOf(integers[i]) + String.valueOf(integers[j]));
+                    jLoop = Integer.parseInt(String.valueOf(integers[i - 1]) + String.valueOf(integers[j - 1]));
                 }
 
-                Random random=new Random();
-                int randomNumber=random.nextInt(9)+1;
+                if (iLoop + jLoop == target) {
+                    System.out.println(iLoop + "+" + jLoop);
+                }
 
+                if (iLoop - jLoop == target) {
+                    System.out.println(iLoop + "-" + jLoop);
+                }
 
-            }else {
-                System.out.println("This number already exists...");
-            }
-
-        }
-    }
-    public boolean checkNumber(int number,int[] computer,int[] player){
-        for (int i=0;i<computer.length;i++){
-            if (number==computer[i]){
-                return false;
+                if (iLoop * jLoop == target) {
+                    System.out.println(iLoop + "*" + jLoop);
+                }
             }
         }
-        for (int i=0;i< player.length;i++){
-            if (number==player[i]){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void addToArray(String[][] array,int number,String symbol){
-        if (number==1){
-            array[0][0]=symbol;
-        }else if (number==2){
-            array[0][2]=symbol;
-        }
-        else if (number==3){
-            array[0][4]=symbol;
-        }
-        else if (number==4){
-            array[1][0]=symbol;
-        }
-        else if (number==5){
-            array[1][2]=symbol;
-        }
-        else if (number==6){
-            array[1][4]=symbol;
-        }
-        else if (number==7){
-            array[2][0]=symbol;
-        }
-        else if (number==8){
-            array[2][2]=symbol;
-        }
-        else if (number==9){
-            array[2][4]=symbol;
-        }
-    }
-
-    public void check(){
-        ArrayList<String> arrayList=new ArrayList<String>();
-        arrayList.add("123");
-        arrayList.add("456");
-        arrayList.add("789");
-        arrayList.add("147");
-        arrayList.add("258");
-        arrayList.add("369");
-        arrayList.add("159");
-        arrayList.add("357");
-
-        for (int i=0;i<arrayList.size();i++){
-            String string=arrayList.get(i);
-            for (int j=0;j<string.length();j++) {
-                int number = Integer.parseInt(String.valueOf(string.charAt(j)));
-
-            }
-        }
-
-
     }
 }
